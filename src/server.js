f48e5c04dadc406511eccda7dfc395b0d6b20af3
Sarahribeiro5 <sarahribeiro.tds2.senai@gmail.com>
2025-04-17 09:52:16 -0300

    feat: create collection model, controller and route

--- src/server.js
+++ src/server.js
@@ -1,15 +1,16 @@
 import express from "express";
 import { config } from "dotenv";
-import cors from "cors"; // Import o middleware CORS
+import cors from "cors"; // Importa o middleware CORS
 
 import animesRouter from "./routes/animeRoutes.js";
-import collectionsRouter from "./routes/collectionRoutes.js";  
+import collectionRouter from "./routes/collectionRoutes.js";
 
 config(); // Carrega variáveis de ambiente do arquivo .env
-const port = process.env.PORT || 4001;
+const port = process.env.PORT || 4001; // Define a porta do servidor
 
 // Inicializa o Express
 const app = express();
+app.use(cors()); // Habilita CORS para todas as rotas
 
 app.use(express.json()); // Parse de JSON
 
